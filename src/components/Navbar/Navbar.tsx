5171a9a361f542afde3145ce5941b505bde8c27d
Tointech <leanhthunk@gmail.com>
2023-08-01 23:20:52 +0700

    add login page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -25,7 +25,8 @@ const Navbar: React.FC<NavbarProps> = ({
         <nav className="flex flex-col w-full">
             <div className="shadow-[0px_4px_30px_0px_rgba(0,_0,_0,_0.08)] bg-white flex flex-row justify-between h-32 shrink-0 items-center pt-6 pl-16 pr-10">
                 <div className="logo">
-                    <img src={logoUrl} className="min-w-0 w-10" />                <span>{name}</span>
+                    <img src={logoUrl} className="min-w-0 w-10" />
+                    <span>{name}</span>
                 </div>
                 <div className="search">
                     <input type="text" placeholder={searchPlaceholder} />
