8f69a2e72eaa954e8564dfe5037d9af4e44dfada
Tointech <leanhthunk@gmail.com>
2023-08-05 04:25:39 +0700

    [UPDATE] navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import GiveButton from './Button/GiveButton';
-import ReceiveButton from './Button/ReceiveButton';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 interface NavbarProps {
@@ -25,27 +23,35 @@ const Navbar: React.FC<NavbarProps> = ({
     return (
         <div className="flex flex-col w-full">
             <div className="shadow-[0px_4px_30px_0px_rgba(0,_0,_0,_0.08)] bg-white flex flex-row justify-between h-32 shrink-0 items-center pt-6 pl-16 pr-10">
-                <div className="logo">
-                    <img src={logoUrl} className="min-w-0 w-10" />
-                </div>
-                <div className="name">
-                    <span >{name}</span>
+                <div className="flex gap-8">
+                    <div className="logo">
+                        <img src={logoUrl} className="mx-auto w-10" />
+                    </div>
+                    <div className="name">
+                        <span >{name}</span>
+                    </div>
                 </div>
 
-                <div className="info-hotline">
-                    <span>Hotline: {hotlineNumber}</span>
+
+                <div className="flex gap-8">
+                    <div className="info-hotline">
+                        <span>Hotline: {hotlineNumber}</span>
+                    </div>
+                    <li onClick={onGiveClick} >Give</li>
+                    <li onClick={onReceiveClick} >Recieve</li>
+                    <li onClick={onLoginClick}>Login</li>
+                    <div className="language">
+                        <select>
+                            <option value="en">Vietnamese</option>
+                            <option value="fr">English</option>
+                            {/* Add more language options as needed */}
+                        </select>
+                    </div>
+                    <li>
+                        <AccountCircleOutlinedIcon className="cursor-pointer" fontSize="large" onClick={onProfileClick} />
+                    </li>
                 </div>
-                <GiveButton onClick={onGiveClick} />
-                <ReceiveButton onClick={onReceiveClick} />
-                <button onClick={onLoginClick}>Login</button>
-                <div className="language">
-                    <select>
-                        <option value="en">Vietnamese</option>
-                        <option value="fr">English</option>
-                        {/* Add more language options as needed */}
-                    </select>
-                </div>
-                <AccountCircleOutlinedIcon className="cursor-pointer" fontSize="large" onClick={onProfileClick} />
+
             </div>
         </div >
     );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -17,136 +17,151 @@ const validationSchema = yup.object({
 });
 
 const LoginPage: React.FC = () => {
-  let navigate = useNavigate();
+  const { loginUser } = useContext(AuthContext);
 
-  const { register, handleSubmit } = useForm<FormData>({ mode: "onChange" })
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
 
-  const onSubmit = handleSubmit(({ email, password, select, remember }) => {
-    console.log(email, password, select, remember)
-  })
+  const { email, password } = loginForm;
 
+  const handleChangeLoginForm = (event: ChangeEvent<HTMLInputElement>) =>
+    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
 
-  if (loginData.success) {
-    // if user logged in
-    navigate("/home");
-  } else {
-    // handle wrong password
-    (() => {
-      alert("Invalid email or password!");
-      setTimeout(() => {
-        const alertElement = document.querySelector(
-          ".alert"
-        ) as HTMLElement;
-        if (alertElement) {
-          alertElement.style.display = "none";
-        }
-      }, 5000);
-    })();
-  }
-} catch (error) {
-  console.log(error);
-}
+  const navigate = useNavigate();
+
+  const login = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const loginData = await loginUser(loginForm);
+
+      if (loginData.success) {
+        // if user logged in
+        navigate("/home");
+      } else {
+        // handle wrong password
+        (() => {
+          alert("Invalid email or password!");
+          setTimeout(() => {
+            const alertElement = document.querySelector(
+              ".alert"
+            ) as HTMLElement;
+            if (alertElement) {
+              alertElement.style.display = "none";
+            }
+          }, 5000);
+        })();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-const {
-  register,
-  // handleSubmit,
-  formState: { errors },
-} = useForm<UserLoginForm>({
-  defaultValues,
-  resolver: yupResolver<UserLoginForm>(validationSchema),
-  mode: "onTouched",
-});
+  const {
+    register,
+    // handleSubmit,
+    formState: { errors },
+  } = useForm<UserLoginForm>({
+    defaultValues,
+    resolver: yupResolver<UserLoginForm>(validationSchema),
+    mode: "onTouched",
+  });
 
-// const onSubmit = handleSubmit(({ email, password, remember }) => {
-//   console.log(email, password, remember);
-// });
+  // const onSubmit = handleSubmit(({ email, password, remember }) => {
+  //   console.log(email, password, remember);
+  // });
 
-const handleRegisterClick = () => {
-  navigate("/signup");
-};
+  const handleRegisterClick = () => {
+    navigate("/signup");
+  };
 
-const handleHomeClick = () => {
-  navigate('/home')
-}
+  const handleHomeClick = () => {
+    navigate("/home");
+  };
 
-return (
-  <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
-    <div className="max-w-md w-full mx-auto">
-      <img
-        className="mx-auto cursor-pointer"
-        onClick={handleHomeClick}
-        src="https://file.rendit.io/n/BMguV6XTfgasPlBI7Wr2.svg"
-      />
-      <div className="text-3xl font-bold text-gray-900 mt-2 text-center">
-        Login Page
+  // const handleForgotPassword = () => {
+  //   // do something here to modify password
+  // };
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
+      <div className="max-w-md w-full mx-auto">
+        <img
+          className="mx-auto cursor-pointer"
+          onClick={handleHomeClick}
+          src="https://file.rendit.io/n/BMguV6XTfgasPlBI7Wr2.svg"
+        />
+        <div className="text-3xl font-bold text-gray-900 mt-2 text-center">
+          Login Page
+        </div>
+      </div>
+      <div className="max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
+        <form action="" className="space-y-6" onSubmit={login}>
+          <div>
+            <label htmlFor="" className="text-sm font-bold text-gray-600 block">
+              Email
+            </label>
+            <input
+              {...register("email")}
+              name="email"
+              type="email"
+              className="w-full p-2 border-gray-300 rounded mt-1"
+              required
+              value={email}
+              onChange={handleChangeLoginForm}
+            />
+            {errors.email && (
+              <p className="error-message text-red-500">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="" className="text-sm font-bold text-gray-600 block">
+              Password
+            </label>
+            <input
+              {...register("password")}
+              name="password"
+              type="password"
+              className="w-full p-2 border-gray-300 rounded mt-1"
+              required
+              value={password}
+              onChange={handleChangeLoginForm}
+            />
+            {errors.password && (
+              <p className="error-message text-red-500">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div className="flex items-center justify-between">
+            <a href="" className="font-medium text-sm text-blue-500">
+              Forgot Password
+            </a>
+          </div>
+          <div>
+            <button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">
+              Submit
+            </button>
+          </div>
+          <div className="flex items-center">
+            <div className="text-sm text-gray-500 text-center pr-1">
+              You don't have an account?
+            </div>
+            <div
+              className="text-sm text-blue-400 hover:text-blue-500 cursor-pointer"
+              onClick={handleRegisterClick}
+            >
+              Signup here
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-    <div className="max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
-      <form action="" className="space-y-6" onSubmit={login}>
-        <div>
-          <label htmlFor="" className="text-sm font-bold text-gray-600 block">
-            Email
-          </label>
-          <input
-            {...register("email")}
-            name="email"
-            type="email"
-            className="w-full p-2 border-gray-300 rounded mt-1"
-            required
-            value={email}
-            onChange={handleChangeLoginForm}
-          />
-          {errors.email && (
-            <p className="error-message text-red-500">
-              {errors.email.message}
-            </p>
-          )}
-        </div>
-        <div>
-          <label htmlFor="" className="text-sm font-bold text-gray-600 block">
-            Password
-          </label>
-          <input
-            {...register("password")}
-            name="password"
-            type="password"
-            className="w-full p-2 border-gray-300 rounded mt-1"
-            required
-            value={password}
-            onChange={handleChangeLoginForm}
-          />
-          {errors.password && (
-            <p className="error-message text-red-500">
-              {errors.password.message}
-            </p>
-          )}
-        </div>
-        <div className="flex items-center justify-between">
-          <a href="" className="font-medium text-sm text-blue-500">
-            Forgot Password
-          </a>
-        </div>
-        <div>
-          <button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">
-            Submit
-          </button>
-        </div>
-        <div className="flex items-center">
-          <div className="text-sm text-gray-500 text-center pr-1">
-            You don't have an account?
-          </div>
-          <div
-            className="text-sm text-blue-400 hover:text-blue-500 cursor-pointer"
-            onClick={handleRegisterClick}
-          >
-            Signup here
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-);
+  );
 };
 
 export default LoginPage;
