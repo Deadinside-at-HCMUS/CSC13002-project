e36640e8c922f40f464c66b7d6367487ec83e1d6
Tointech <leanhthunk@gmail.com>
2023-08-08 20:33:24 +0700

    [ADD] statistics grid

---
+++ src/components/Chart/StatsGrid.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { FiUsers } from 'react-icons/fi'
+import { BsBoxArrowInDown, BsBoxArrowUp } from 'react-icons/bs'
+import { VscDebugDisconnect } from 'react-icons/vsc'
+
+interface BoxWrapperProps {
+    children: React.ReactNode;
+}
+
+interface StatisticsProps {
+    totalDonations: number;
+    totalReceives: number;
+    totalUsers: number;
+    totalConnections: number;
+}
+
+function BoxWrapper({ children }: BoxWrapperProps) {
+    return <div className="bg-white rounded-md p-4 flex-1 border border-gray-200 flex items-center">{children}</div>
+}
+
+const StatsGrid: React.FC<StatisticsProps> = ({
+    totalDonations,
+    totalReceives,
+    totalUsers,
+    totalConnections,
+}) => {
+    return (
+        <div className="flex gap-4">
+            <BoxWrapper>
+                <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+                    <BsBoxArrowInDown className="text-2xl text-white" />
+                </div>
+                <div className="pl-4">
+                    <span className="text-sm text-gray-500 font-light">Total Donations</span>
+                    <div className="flex items-center">
+                        <strong className="text-xl text-gray-700 font-semibold">{totalDonations}</strong>
+                        <span className="text-sm text-green-500 pl-2">+2</span>
+                    </div>
+                </div>
+            </BoxWrapper>
+            <BoxWrapper>
+                <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+                    <BsBoxArrowUp className="text-2xl text-white" />
+                </div>
+                <div className="pl-4">
+                    <span className="text-sm text-gray-500 font-light">Total Receives</span>
+                    <div className="flex items-center">
+                        <strong className="text-xl text-gray-700 font-semibold">{totalReceives}</strong>
+                        <span className="text-sm text-red-500 pl-2">-2</span>
+                    </div>
+                </div>
+            </BoxWrapper>
+            <BoxWrapper>
+                <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+                    <FiUsers className="text-2xl text-white" />
+                </div>
+                <div className="pl-4">
+                    <span className="text-sm text-gray-500 font-light">Total Users</span>
+                    <div className="flex items-center">
+                        <strong className="text-xl text-gray-700 font-semibold">{totalUsers}</strong>
+                        <span className="text-sm text-green-500 pl-2">+5</span>
+                    </div>
+                </div>
+            </BoxWrapper>
+            <BoxWrapper>
+                <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
+                    <VscDebugDisconnect className="text-2xl text-white" />
+                </div>
+                <div className="pl-4">
+                    <span className="text-sm text-gray-500 font-light">Total Connection</span>
+                    <div className="flex items-center">
+                        <strong className="text-xl text-gray-700 font-semibold">{totalConnections}</strong>
+                        <span className="text-sm text-green-500 pl-2">+1</span>
+                    </div>
+                </div>
+            </BoxWrapper>
+        </div>
+    )
+};
+
+export default StatsGrid;
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import StatsGrid from "../Chart/StatsGrid";
 
 const Dashboard: React.FC = () => {
     return (
-        <div className="">
-            Dashboard
+        <div className="flex flex-col gap-4">
+            <StatsGrid
+                totalDonations={21}
+                totalReceives={23}
+                totalUsers={72}
+                totalConnections={50}
+            />
         </div>
     )
 };
