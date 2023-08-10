512f06f2dd6a80158fb647a9ae52db59686eba44
ng-minhdat <nmdat21@clc.fitus.edu.vn>
2023-08-10 11:53:42 +0700

    [UPDATE] allow a post to have multiple items

--- server/models/post.js
+++ server/models/post.js
@@ -2,87 +2,152 @@ const joi = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Create a Post schema
-const postSchema = new mongoose.Schema(
-  {
-    type: {
-      type: String,
-      enum: ["Donating", "Receiving"],
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    body: {
-      type: String,
-      required: true,
-    },
-    // photo: {
-    //   type: String, // Assuming you store the path or URL of the image
-    // },
-    author: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
-    item: {
-      type: String,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    category: {
-      type: String,
-      enum: ["Electronic", "Clothing", "Book", "Food", "Vehicle", "Household"],
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["Published", "Verified", "Matching", "Matched"],
-      default: "Published",
-      required: true,
-    },
-    location: {
-      type: String,
-      required: true,
-    },
-    match: {
-      type: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "post",
+const itemSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            // required: true,
         },
-      ],
-      default: [],
+        quantity: {
+            type: Number,
+            // required: true,
+        },
+        category: {
+            type: String,
+            enum: [
+                "Electronic",
+                "Clothing",
+                "Book",
+                "Food",
+                "Vehicle",
+                "Household",
+                "Medical",
+            ],
+            // required: true,
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
+);
+
+const postSchema = new mongoose.Schema(
+    {
+        type: {
+            type: String,
+            enum: ["Donating", "Receiving"],
+            // required: true,
+        },
+        title: {
+            type: String,
+            // required: true,
+        },
+        body: {
+            type: String,
+            // required: true,
+        },
+        // photo: {
+        //   type: String, // Assuming you store the path or URL of the image
+        // },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+            // required: true,
+        },
+        item: {
+            type: [itemSchema],
+            // required: true,
+        },
+        // item: {
+        //     type: String,
+        //     required: true,
+        // },
+        // quantity: {
+        //     type: Number,
+        //     required: true,
+        // },
+        // category: {
+        //     type: String,
+        //     enum: [
+        //         "Electronic",
+        //         "Clothing",
+        //         "Book",
+        //         "Food",
+        //         "Vehicle",
+        //         "Household",
+        //     ],
+        //     required: true,
+        // },
+        status: {
+            type: String,
+            enum: ["Published", "Verified", "Matching", "Matched"],
+            default: "Published",
+            // required: true,
+        },
+        location: {
+            type: String,
+            // required: true,
+        },
+        match: {
+            type: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: "post",
+                },
+            ],
+            default: [],
+        },
+    },
+    { timestamps: true }
 );
 
 const validate = (post) => {
-  const schema = joi.object({
-    type: joi.string().valid("Donating", "Receiving").required(),
-    title: joi.string().required(),
-    body: joi.string().required(),
-    author: joi.string().allow(""),
-    item: joi.string().required(),
-    quantity: joi.number().greater(0).required(),
-    category: joi
-      .string()
-      .valid("Electronic", "Clothing", "Book", "Food", "Vehicle", "Household")
-      .required(),
-    status: joi
-      .string()
-      .valid("Published", "Verified", "Matching", "Matched")
-      .default("Published")
-      .allow(""),
-    location: joi.string().required(),
-    match: joi.array().default([]).optional(),
-  });
-  return schema.validate(post);
+    const itemSchema = {
+        name: joi.string().required(),
+        quantity: joi.number().greater(0).required(),
+        category: joi
+            .string()
+            .valid(
+                "Electronic",
+                "Clothing",
+                "Book",
+                "Food",
+                "Vehicle",
+                "Household",
+                "Medical"
+            )
+            .required(),
+    };
+
+    const singleItemSchema = joi.object().keys(itemSchema);
+
+    const schema = joi.object({
+        type: joi.string().valid("Donating", "Receiving").required(),
+        title: joi.string().required(),
+        body: joi.string().required(),
+        author: joi.string().allow(""),
+        // item: joi.string().required(),
+        // quantity: joi.number().greater(0).required(),
+        // category: joi
+        //     .string()
+        //     .valid(
+        //         "Electronic",
+        //         "Clothing",
+        //         "Book",
+        //         "Food",
+        //         "Vehicle",
+        //         "Household"
+        //     )
+        //     .required(),
+        // item: joi.object().keys(itemSchema).unknown(true),
+        item: joi.array().items(singleItemSchema),
+        status: joi
+            .string()
+            .valid("Published", "Verified", "Matching", "Matched")
+            .default("Published")
+            .allow(""),
+        location: joi.string().required(),
+        match: joi.array().default([]).optional(),
+    });
+    return schema.validate(post);
 };
 
 const Post = mongoose.model("post", postSchema);
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+// const { Post } = require("../models/post");
 const { Post, validate } = require("../models/post");
 const { User } = require("../models/user");
 const verifyToken = require("../middleware/auth");
@@ -9,93 +10,108 @@ const validateObjectId = require("../middleware/validObjectId");
 // @desc Create post
 // @access Private
 router.post("/", verifyToken, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error)
-    return res.status(400).json({
-      success: false,
-      message: error.details[0].message,
+    const { error } = validate(req.body);
+    if (error)
+        return res.status(400).json({
+            success: false,
+            message: error.details[0].message,
+        });
+
+    const user = await User.findById(req.userId);
+
+    if (req.body.type == "Receiving") {
+        if (req.body.match && req.body.match.length > 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Only donating post can access this field",
+            });
+        }
+    }
+
+    const newPost = await Post({ ...req.body, author: user._id }).save();
+
+    // res.status(201).send({ data: newPost });
+    res.status(200).json({
+        success: true,
+        content: newPost,
+        message: "A new post has been created",
     });
-
-  const user = await User.findById(req.userId);
-  const newPost = await Post({ ...req.body, author: user._id }).save();
-
-  res.status(201).send({ data: newPost });
 });
 
 // @route GET api/post
 // @desc Get post
 // @access Private
 router.get("/", verifyToken, async (req, res) => {
-  const posts = await Post.find({ author: req.userId })
-    .populate("author", "username email")
-    .populate("match");
-  return res.json({ success: true, posts });
+    const posts = await Post.find({ author: req.userId })
+        .populate("author", "username email")
+        .populate("match");
+    return res.json({ success: true, posts });
 });
 
 // @route PUT api/post
 // @desc Update post
 // @access Private
 router.put("/:id", [validateObjectId, verifyToken], async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
+    try {
+        const post = await Post.findById(req.params.id);
 
-    // Check if the post exists
-    if (!post) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Post not found" });
+        // Check if the post exists
+        if (!post) {
+            return res
+                .status(404)
+                .json({ success: false, message: "Post not found" });
+        }
+
+        const user = await User.findById(req.userId);
+        if (!user._id.equals(post.author))
+            return res
+                .status(403)
+                .send({ message: "User don't have access to edit!" });
+
+        let updatedPost = {
+            type: req.body.type || post.type,
+            title: req.body.title || post.title,
+            body: req.body.body || post.body,
+            item: req.body.item || post.item,
+            // quantity: req.body.quantity || post.quantity,
+            // category: req.body.category || post.category,
+            status: req.body.status || post.status,
+            location: req.body.location || post.location,
+            match: req.body.match || post.match,
+        };
+
+        const { error } = validate(updatedPost);
+        if (error)
+            return res.status(400).send({ message: error.details[0].message });
+
+        const postUpdateCondition = { _id: req.params.id, author: req.userId };
+
+        updatedPost = await Post.findOneAndUpdate(
+            postUpdateCondition,
+            updatedPost,
+            {
+                new: true,
+            }
+        );
+
+        // User not authorised to update post or post not found
+        if (!updatedPost)
+            return res.status(401).json({
+                success: false,
+                message: "Post not found or user not authorised",
+            });
+
+        res.json({
+            success: true,
+            message: "Update successfully!",
+            post: updatedPost,
+        });
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ success: false, message: "An error occurred" });
     }
-
-    const user = await User.findById(req.userId);
-    if (!user._id.equals(post.author))
-      return res
-        .status(403)
-        .send({ message: "User don't have access to edit!" });
-
-    let updatedPost = {
-      type: req.body.type || post.type,
-      title: req.body.title || post.title,
-      body: req.body.body || post.body,
-      item: req.body.item || post.item,
-      quantity: req.body.quantity || post.quantity,
-      category: req.body.category || post.category,
-      status: req.body.status || post.status,
-      location: req.body.location || post.location,
-      match: req.body.match || post.match,
-    };
-
-    const { error } = validate(updatedPost);
-    if (error)
-      return res.status(400).send({ message: error.details[0].message });
-
-    const postUpdateCondition = { _id: req.params.id, author: req.userId };
-
-    updatedPost = await Post.findOneAndUpdate(
-      postUpdateCondition,
-      updatedPost,
-      {
-        new: true,
-      }
-    );
-
-    // User not authorised to update post or post not found
-    if (!updatedPost)
-      return res.status(401).json({
-        success: false,
-        message: "Post not found or user not authorised",
-      });
-
-    res.json({
-      success: true,
-      message: "Excellent progress!",
-      post: updatedPost,
-    });
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ success: false, message: "An error occurred" });
-  }
 });
 
 module.exports = router;
