7b7e75458d7590780a26b9c302cc195121f5f999
Tointech <leanhthunk@gmail.com>
2023-08-16 19:13:41 +0700

    [UPDATE] search bar

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch, AiOutlineCloseCircle } from "react-icons/ai";
-import { BsHouseDoor } from "react-icons/bs";
-import { CiLocationOn } from "react-icons/ci";
 
 interface SearchProps {
     onTypeSelect: (e: React.ChangeEvent<HTMLSelectElement>) => void;
@@ -16,36 +14,29 @@ const Search: React.FC<SearchProps> = ({
     onLocationSelect,
     onClearAll,
 }) => {
+    const [searchPost, setSearchPost] = useState("");
+
+    const handleClearPost = () => {
+        setSearchPost("");
+    };
+
     return (
         <div className="grid gap-10 bg-[#f1f4f8] rounded-[10px] p-[3rem]">
             <form action="">
                 <div className="flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-[#f1f4f8]-700">
-                    <div className="flex gap-2 items-center">
+                    <div className="flex items-center gap-2 flex-grow">
                         <AiOutlineSearch className="text-[25px] icon" />
                         <input
                             type="text"
-                            className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+                            className="bg-transparent text-blue-500 focus:outline-none flex-grow"
                             placeholder="Search Post Here..."
+                            value={searchPost}
+                            onChange={(e) => setSearchPost(e.target.value)}
                         />
-                        <AiOutlineCloseCircle className="text-[25px] text-[#a5a6a6] hover:text-[#000] icon" />
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <BsHouseDoor className="text-[25px] icon" />
-                        <input
-                            type="text"
-                            className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
-                            placeholder="Search Donator Here..."
+                        <AiOutlineCloseCircle
+                            className="text-[20px] text-[#a5a6a6] hover:text-[#000] icon mr-2"
+                            onClick={handleClearPost}
                         />
-                        <AiOutlineCloseCircle className="text-[25px] text-[#a5a6a6] hover:text-[#000] icon" />
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <CiLocationOn className="text-[25px] icon" />
-                        <input
-                            type="text"
-                            className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
-                            placeholder="Search Location Here..."
-                        />
-                        <AiOutlineCloseCircle className="text-[25px] text-[#a5a6a6] hover:text-[#000] icon" />
                     </div>
                     <button className="bg-blue-500 h-full p-3 px-6 rounded-[10px] text-white cursor-pointer hover:bg-blue-300">
                         Search
